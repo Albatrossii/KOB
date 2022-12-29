45869c98374a06c398916a41355099d040f0b82a
Albatross <111957077+Albatrossii@users.noreply.github.com>
2022-12-29 21:32:53 +0800

    创建部分个人中心页面

--- backend/src/main/java/com/kob/backend/service/impl/user/account/InfoServiceImpl.java
+++ backend/src/main/java/com/kob/backend/service/impl/user/account/InfoServiceImpl.java
@@ -1,8 +1,10 @@
 package com.kob.backend.service.impl.user.account;
 
+import com.kob.backend.Utils.UserUtil;
 import com.kob.backend.pojo.User;
 import com.kob.backend.service.impl.utils.UserDetailsImpl;
 import com.kob.backend.service.user.account.InfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -12,12 +14,13 @@ import java.util.Map;
 
 @Service
 public class InfoServiceImpl implements InfoService {
+
+    private  UserUtil util;
+
     @Override
     public Map<String, String> getInfo() {
-        UsernamePasswordAuthenticationToken authenticationToken=
-                (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-        UserDetailsImpl loginuser = (UserDetailsImpl) authenticationToken.getPrincipal();
-        User user = loginuser.getUser();
+
+        User user = util.getUser();
 
         Map<String,String> map = new HashMap<>();
         map.put("error_message","success");
--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -6,11 +6,88 @@
 
 <script>
 import ContentField from "../../../components/ContentField.vue";
+import { useStore } from "vuex";
+import $ from 'jquery'
 
 export default{
     components:{
         ContentField,
+    },
+    setup() {
+        // const store = useStore();
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/add/",
+        //     type: "post",
+        //     data: {
+        //         title: "Bot标题",
+        //         description: "Bot描述",
+        //         content: "Bot代码",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer "+ store.state.user.token ,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     error(resp){
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/remove/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 1,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer "+store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     error(resp){
+        //         console.log(resp);
+        //     }
+        // })
+
+        // const store = useStore();
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/update/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 5,
+        //         title: "Bot新标题",
+        //         description: "Bot新描述",
+        //         content: "Bot新代码",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer "+ store.state.user.token ,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     error(resp){
+        //         console.log(resp);
+        //     }
+        // })
+
+        const store = useStore();
+        $.ajax({
+            url: "http://127.0.0.1:3000/user/bot/getlist/",
+            type: "get",
+            headers: {
+                Authorization: "Bearer "+ store.state.user.token ,
+            },
+            success(resp){
+                console.log(resp);
+            },
+            error(resp){
+                console.log(resp);
+            }
+        })
     }
+
 }
 </script>
 
